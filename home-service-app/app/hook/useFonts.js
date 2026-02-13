f8ed67364f0e8b3a28cf564903f3fa0e008f5b1b
Amit Gangrade <agangrade5>
2026-02-13 16:36:38 +0530

    Home service app - custom fonts implements.

--- home-service-app/app/hook/useFonts.js
+++ home-service-app/app/hook/useFonts.js
@@ -1,13 +1,13 @@
 import { useFonts } from "expo-font";
 
 export default function useCustomFonts() {
-  const [fontsLoaded] = useFonts({
-    OutfitBold: require("../../assets/fonts/Outfit-Bold.ttf"),
-    OutfitSemiBold: require("../../assets/fonts/Outfit-SemiBold.ttf"),
-    OutfitMedium: require("../../assets/fonts/Outfit-Medium.ttf"),
-    OutfitRegular: require("../../assets/fonts/Outfit-Regular.ttf"),
-    OutfitLight: require("../../assets/fonts/Outfit-Light.ttf"),
-  });
+    const [fontsLoaded] = useFonts({
+        OutfitBold: require("../../assets/fonts/Outfit-Bold.ttf"),
+        OutfitSemiBold: require("../../assets/fonts/Outfit-SemiBold.ttf"),
+        OutfitMedium: require("../../assets/fonts/Outfit-Medium.ttf"),
+        OutfitRegular: require("../../assets/fonts/Outfit-Regular.ttf"),
+        OutfitLight: require("../../assets/fonts/Outfit-Light.ttf"),
+    });
 
-  return fontsLoaded;
+    return fontsLoaded;
 }
